b16c8c93cfb82b3053c1b019fede72979319de50
Dev-bu-tch-er <trevor254oduol@gmail.com>
2025-08-08 09:40:25 +0300

    reverted the changes

--- config/ingest.js
+++ config/ingest.js
@@ -17,7 +17,7 @@ export const syncUserCreation = inngest.createFunction(
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
     const userData = {
-      id: id,
+      _id: id,
       email: email_addresses[0].email_address,
       name: first_name + "" + last_name,
       imageUrl: image_url,
@@ -40,7 +40,7 @@ export const syncUserUpdation = inngest.createFunction(
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
     const userData = {
-      id: id,
+      _id: id,
       email: email_addresses[0].email_address,
       name: first_name + "" + last_name,
       imageUrl: image_url,
--- models/User.js
+++ models/User.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    _id: {
-      type: mongoose.Schema.Types.ObjectId,
-      auto: true,
-    },
+    _id: { type: String, required: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     imageUrl: { type: String, required: true },
